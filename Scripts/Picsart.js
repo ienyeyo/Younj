4430f82ec07828da583d9aead8f9c9f36594b7c3
ien yeyo <jungie.ien@gmail.com>
2026-06-30 12:15:32 +0700

    Add files via upload

---
+++ Scripts/Picsart.js
@@ -0,0 +1,23 @@
+/***********************************************
+ > 𝐀𝐮𝐭𝐡𝐨𝐫:  𝑨𝒅𝒎𝒊𝒏: 𝒀𝒐𝒖𝒏𝒋 𝑶𝑺 🎨
+***********************************************/
+// 📌 𝐍𝐨𝐭𝐞: 𝐓𝐡𝐢𝐬 𝐌𝐨𝐝𝐮𝐥𝐞,𝐬𝐜𝐫𝐢𝐩𝐭.𝐣𝐬 𝐟𝐢𝐥𝐞 𝐢𝐬 𝐜𝐫𝐞𝐚𝐭𝐞𝐝 𝐛𝐲 𝐊𝐇𝐎𝐍𝐆 𝐌𝐀𝐍𝐇 𝐘𝐄𝐍. 
+// 𝐏𝐥𝐞𝐚𝐬𝐞 𝐝𝐨 𝐧𝐨𝐭 𝐞𝐝𝐢𝐭 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐩𝐞𝐫𝐦𝐢𝐬𝐬𝐢𝐨𝐧. 𝐓𝐡𝐚𝐧𝐤 𝐲𝐨𝐮 🩵
+// 𝐋𝐮̛𝐮 𝐲́ 𝐤𝐡𝐢 𝐬𝐮̛̉ 𝐝𝐮̣𝐧𝐠: ⚠️𝐃𝐨𝐚̣𝐧 𝐦𝐚̃ 𝐧𝐚̀𝐲 𝐜𝐡𝐢̉ 𝐝𝐚̀𝐧𝐡 𝐜𝐡𝐨 𝐦𝐮̣𝐜 𝐝𝐢́𝐜𝐡 𝐦𝐨̛̉ 𝐤𝐡𝐨𝐚́ 𝐯𝐢𝐩 𝐜𝐡𝐨 𝐮̛́𝐧𝐠 𝐝𝐮̣𝐧𝐠. 𝐕𝐮𝐢 𝐥𝐨̀𝐧𝐠 𝐤𝐡𝐨̂𝐧𝐠 𝐬𝐚𝐨 𝐜𝐡𝐞́𝐩 𝐡𝐨𝐚̣̆𝐜 𝐛𝐚́𝐧 𝐥𝐚̣𝐢! ❗️
+// 🫆 𝐋𝐢𝐧𝐤 𝐛𝐢𝐨 𝐭𝐚̉𝐢 𝐱𝐮𝐨̂́𝐧𝐠: 𝐡𝐭𝐭𝐩𝐬://𝐲𝐨𝐮𝐧𝐣.𝐢𝐨.𝐯𝐧/
+/******************************
+𝑨𝒅𝒎𝒊𝒏: 𝒀𝒐𝒖𝒏𝒋 𝑶𝑺 🎭
+𝑭𝒂𝒄𝒆𝒃𝒐𝒐𝒌:𝑲𝒉𝒐𝒏𝒈 𝑴𝒂𝒏𝒉 𝒀𝒆𝒏 👑 
+𝑪𝒐𝒏𝒕𝒂𝒄𝒕:
+𝒛𝒍:𝟎𝟗𝟖𝟏.𝟑𝟎𝟗.𝟗𝟐𝟏
+𝑻𝒆𝒍𝒆𝒈𝒂𝒎: 𝒚𝒐𝒖𝒏𝒋_𝒊𝒄𝒍𝒐𝒖𝒅
+*******************************/
+function _0x5ade(_0x48c94d,_0x5bf9d8){_0x48c94d=_0x48c94d-0x168;const _0x3a790e=_0x3a79();let _0x5ade53=_0x3a790e[_0x48c94d];return _0x5ade53;}const _0x241aba=_0x5ade;(function(_0xb3ff9b,_0xe1950b){const _0xc6d8a6=_0x5ade,_0xdcb41c=_0xb3ff9b();while(!![]){try{const _0x27e63f=-parseInt(_0xc6d8a6(0x169))/0x1+-parseInt(_0xc6d8a6(0x176))/0x2+-parseInt(_0xc6d8a6(0x172))/0x3*(parseInt(_0xc6d8a6(0x177))/0x4)+-parseInt(_0xc6d8a6(0x175))/0x5*(-parseInt(_0xc6d8a6(0x17d))/0x6)+parseInt(_0xc6d8a6(0x174))/0x7*(-parseInt(_0xc6d8a6(0x17a))/0x8)+parseInt(_0xc6d8a6(0x17c))/0x9*(parseInt(_0xc6d8a6(0x16d))/0xa)+parseInt(_0xc6d8a6(0x16a))/0xb;if(_0x27e63f===_0xe1950b)break;else _0xdcb41c['push'](_0xdcb41c['shift']());}catch(_0x113300){_0xdcb41c['push'](_0xdcb41c['shift']());}}}(_0x3a79,0xd1d2c));let objc={'status':_0x241aba(0x168),'response':[{'status':_0x241aba(0x16b),'is_trial':![],'order_id':_0x241aba(0x173),'expire_date':0x3bd5583c000,'purchase_date':0x1965acb6000,'subscription_id':_0x241aba(0x17b),'original_order_id':_0x241aba(0x178),'plan_meta':{'permissions':[_0x241aba(0x16e),_0x241aba(0x16c)],'auto_renew_product_id':_0x241aba(0x16f),'level':0x7d0,'storage_limit_in_mb':0x19000,'id':_0x241aba(0x17b),'frequency':_0x241aba(0x170),'type':_0x241aba(0x171),'scope_id':_0x241aba(0x179),'product_id':'com.picsart.studio.subscription_pro_3_yearly','tier_id':'pro'},'limitation':{'max_count':0xa,'limits_exceeded':![]},'is_eligible_for_introductory':![],'reason':'ok'}]};function _0x3a79(){const _0x3a779e=['279UMfGXK','212886VsBwon','success','609768JQjeUj','22386749WUHOmo','SUBSCRIPTION_RENEWED','premium_tools_ai','435280cDPSen','premium_tools_standard','com.picsart.studio.subscription_mac_tier1_pro_yearly','yearly','renewable','4038ASQjTd','2000001108782321','273MVkpiJ','135jGwcub','2047844BoHffq','4120mCwFsf','2000000756285050','full','94976PCIURY','com.picsart.studio.subscription_pro_3_yearly'];_0x3a79=function(){return _0x3a779e;};return _0x3a79();}$done({'response':{'body':JSON['stringify'](objc),'status':0xc8}});
+/******************************
+Tính năng chính của JavaScript: Picsart Unlock premium features for free! Edit photos like a pro with advanced tools, AI-powered effects, and exclusive filters. Create stunning collages and easily remove backgrounds.
+Link bio tải xuống: https://younj.io.vn
+Phiên bản JavaScript: v3.4
+Cập nhật: ngày 30 tháng 6 năm 2026
+Vui lòng liên hệ: Facebook:Khong Manh Yen👑
+𝐋𝐮̛𝐮 𝐲́ 𝐤𝐡𝐢 𝐬𝐮̛̉ 𝐝𝐮̣𝐧𝐠: ⚠️𝐃𝐨𝐚̣𝐧 𝐦𝐚̃ 𝐧𝐚̀𝐲 𝐜𝐡𝐢̉ 𝐝𝐚̀𝐧𝐡 𝐜𝐡𝐨 𝐦𝐮̣𝐜 𝐝𝐢́𝐜𝐡 𝐦𝐨̛̉ 𝐤𝐡𝐨𝐚́ 𝐯𝐢𝐩 𝐜𝐡𝐨 𝐮̛́𝐧𝐠 𝐝𝐮̣𝐧𝐠. 𝐕𝐮𝐢 𝐥𝐨̀𝐧𝐠 𝐤𝐡𝐨̂𝐧𝐠 𝐬𝐚𝐨 𝐜𝐡𝐞́𝐩 𝐡𝐨𝐚̣̆𝐜 𝐛𝐚́𝐧 𝐥𝐚̣𝐢! ❗️
+*******************************/
